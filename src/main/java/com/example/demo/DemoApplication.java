8039be19e2b2a5bc0cd64d3947c01b441d19bf5b
JaimeDevS <jaime_mendes91@yahoo.com.br>
2025-10-22 21:08:54 -0300

    Test: teste

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
 	@GetMapping("/")
 	public String home() {
-		return "Hello Jenkins CI/CD!";
+		return "Hello Jenkins CI/CD! teste";
 	}
 
 	public static void main(String[] args) {
